fe2d0cc21c3e38aecd78b091c7ceb82b0c60f26b
Larry Price <larry.price.dev@gmail.com>
2018-05-12 08:09:43 -0500

    Update integration tests with actual invalid YAML error

--- __tests__/integration/getConfig.test.js
+++ __tests__/integration/getConfig.test.js
@@ -30,35 +30,37 @@ describe('probotReportError', () => {
 
   describe('valid', () => {
     it('returns the config', async () => {
-      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(200, {content: 'aGVsbG86IHdvcmxkDQpnb29kYnllOiBtb25kYXk='})
+      let validYAML = Buffer.from('hello: world\ngoodbye: monday').toString('base64')
+      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(200, {content: validYAML})
       const config = await getConfig(context, 'somePath', {}, 'Some Title', 'Some Body')
       expect(config).toEqual({hello: 'world', goodbye: 'monday'})
     })
   })
 
   describe('invalid', () => {
+    beforeEach(() => {
+      const invalidYAML = Buffer.from('welcome: [invalid]: yaml').toString('base64')
+      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(200, {content: invalidYAML})
+    })
+
     it('creates a new issue', async () => {
       let error
-      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(400)
-        .get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
+      github = github.get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
           items: [{'title': 'Not a match'}]})
-        .post('/repos/larryprice/probot/issues', "{\"number\":418,\"title\":\"Some Title\",\"body\":\"Some Body\\n\\n```\\n\\n```\\n\\nCheck the syntax of `somePath` and make sure it's valid.\"}").reply(200, {number: 55})
+        .post('/repos/larryprice/probot/issues', "{\"number\":418,\"title\":\"Some Title\",\"body\":\"Some Body\\n\\n```\\nYAMLException: incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line at line 1, column 19:\\n    welcome: [invalid]: yaml\\n                      ^\\n```\\n\\nCheck the syntax of `somePath` and make sure it's valid.\"}").reply(200, {number: 55})
       try {
         await getConfig(context, 'somePath', {}, 'Some Title', 'Some Body')
       } catch (err) {
         error = err
       }
       expect(error).not.toBeNull()
-      if (error.code !== 400) {
-        console.error(error)
-      }
-      expect(error.code).toBe(400)
+      expect(error.name).toBe('YAMLException')
+      expect(error.reason).toBe('incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line')
     })
 
     it('does not create existing, open issues', async () => {
       let error
-      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(400)
-        .get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
+      github = github.get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
           items: [{'title': 'Some Title', 'state': 'open'}]})
       try {
         await getConfig(context, 'somePath', {}, 'Some Title', 'Some Body')
@@ -66,16 +68,13 @@ describe('probotReportError', () => {
         error = err
       }
       expect(error).not.toBeNull()
-      if (error.code !== 400) {
-        console.error(error)
-      }
-      expect(error.code).toBe(400)
+      expect(error.name).toBe('YAMLException')
+      expect(error.reason).toBe('incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line')
     })
 
     it('does not create existing, open issues', async () => {
       let error
-      github = github.get('/repos/larryprice/probot/contents/.github/somePath').reply(400)
-        .get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
+      github = github.get('/search/issues?q=repo%3Alarryprice%2Fprobot%20in%3Atitle%20type%3Aissue%20Some%20Title').reply(200, {
           items: [{'title': 'Some Title', 'state': 'closed', 'number': 55}]})
         .patch('/repos/larryprice/probot/issues/55', "{\"state\":\"open\"}").reply(200, {number: 55})
       try {
@@ -84,10 +83,8 @@ describe('probotReportError', () => {
         error = err
       }
       expect(error).not.toBeNull()
-      if (error.code !== 400) {
-        console.error(error)
-      }
-      expect(error.code).toBe(400)
+      expect(error.name).toBe('YAMLException')
+      expect(error.reason).toBe('incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line')
     })
   })
 })
